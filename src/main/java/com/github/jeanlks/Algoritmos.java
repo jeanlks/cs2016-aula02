6d64dd8da96da107785cb902a3d0544ad991e296
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-09-01 20:18:42 -0300

    Algoritmo 4 - Potencia

--- src/main/java/com/github/jeanlks/Algoritmos.java
+++ src/main/java/com/github/jeanlks/Algoritmos.java
@@ -1,20 +1,26 @@
 package com.github.jeanlks;
+
 /**
- * Algoritmo 2 definido como exercicio para aula2. 
+ * Algoritmos definidos como exercicio para aula2.
  */
 public class Algoritmos {
-
+	/**
+	 * @param i
+	 *            serve para variável de iteração.
+	 * @param s
+	 *            mantém a soma da conta a cada ciclo de i.
+	 * @param n
+	 *            número que se deseja saber a soma dos naturais
+	 * 
+	 * @return soma dos números naturais até n
+	 */
 	public static int somaNaturais(int n) {
-		/**
-		 * @param i serve para variável de iteração.
-		 * @param s mantém a soma da conta a cada ciclo de i.
-		 * @param n número que se deseja saber a soma dos naturais
-		 * 
-		 * @return s soma dos números naturais até n
-		 */
+		if(n<=0){
+			 throw new IllegalArgumentException("numero menor que zero");
+		}
 		int i = 2;
 		int s = 1;
-		
+
 		while (i <= n) {
 			s = s + i;
 			i++;
@@ -22,39 +28,61 @@ public class Algoritmos {
 		return s;
 	}
 
+	/**
+	 * @param a
+	 *            número de parcelas
+	 * @param b
+	 *            valor da parcela
+	 * @param i
+	 *            variável de iteração
+	 * @param s
+	 *            variável de soma
+	 * 
+	 * @return resultado do produto
+	 */
 	public static int produto(int a, int b) {
-		/**
-		 * @param a número de parcelas
-		 * @param b valor da parcela
-		 * @param i variável de iteração
-		 * @param s variável de soma
-		 * 
-		 * @return s resultado do produto
-		 */
+		if(a<=0 || b<=0){
+			 throw new IllegalArgumentException("numero menor que zero");
+		}
 		int totalParcelas = a;
 		int parcela = b;
-		if(b<a){
+		if (b < a) {
 			totalParcelas = b;
 			parcela = a;
 		}
-		
+
 		int i = 1;
 		int s = 0;
-		while(i<=totalParcelas){
+		while (i <= totalParcelas) {
 			s = s + parcela;
 			i++;
 		}
 		return s;
 	}
-	
-	public int potencia(int x, int y){
+
+	/**
+	 * @param x
+	 *            número natural
+	 * @param y
+	 *            valor da potencia
+	 * @param i
+	 *            variável de iteração
+	 * @param potencia
+	 *            variável para valor de potencia
+	 * 
+	 * @return resultado da potencia
+	 */
+	public static int potencia(int x, int y) {
+		if(x<=0 || y<=0){
+			 throw new IllegalArgumentException("numero menor que zero");
+		}
 		int potencia = 1;
 		int i = 1;
-		while(i<=y){
-			potencia = produto(potencia,x);
+		while (i <= y) {
+			potencia = produto(potencia, x);
 			i++;
 		}
 		return potencia;
 	}
-	
+
 }
--- src/test/java/com/github/jeanlks/AlgoritmosTest.java
+++ src/test/java/com/github/jeanlks/AlgoritmosTest.java
@@ -10,19 +10,51 @@ public class AlgoritmosTest {
 	public void numeroCertoParaSoma() {
 		assertEquals(6, Algoritmos.somaNaturais(3));
 	}
-	
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenoresQueZeroSomaNatural() {
+		Algoritmos.somaNaturais(0);
+
+	}
+
 	@Test
 	public void numeroCertoParaProduto() {
-		assertEquals(9, Algoritmos.produto(3,3));
+		assertEquals(9, Algoritmos.produto(3, 3));
+	}
+
+	@Test
+	public void numeroAMaiorQueB() {
+		assertEquals(15, Algoritmos.produto(5, 3));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenoresQueZeroSomaProdutoValorA() {
+		Algoritmos.produto(0, 2);
 	}
 	
-	@Test
-	public void numeroAMaiorQueB(){
-		assertEquals(15, Algoritmos.produto(5,3));
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenoresQueZeroSomaProdutoValorB() {
+		Algoritmos.produto(3, 0);
 	}
-	 @Test
-	    public void apenasParaAgradarJacocoTool100PorCentoCobertura() {
-	        new Algoritmos();
-	    }
-}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenoresQueZeroPotenciaValorX() {
+
+		Algoritmos.potencia(0, 2);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenoresQueZeroPotenciaValorY() {
+
+		Algoritmos.potencia(3, 0);
+	}
+	@Test 
+	public void numeroCertoParaPotencia(){
+		assertEquals(27, Algoritmos.potencia(3, 3));
+	}
+
+	@Test
+	public void apenasParaAgradarJacocoTool100PorCentoCobertura() {
+		new Algoritmos();
+	}
+}	
