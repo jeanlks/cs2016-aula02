967d1e833313d17f2caf6c72d5d64cc8c413372d
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-08-31 21:20:29 -0300

    pom.xml atualizado

---
+++ src/main/java/com/github/jeanlks/Algoritmo2.java
@@ -0,0 +1,7 @@
+package com.github.jeanlks;
+
+public class Algoritmo2 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/com/github/jeanlks/Algoritmo2Test.java
@@ -0,0 +1,5 @@
+package com.github.jeanlks;
+
+public class Algoritmo2Test {
+
+}
