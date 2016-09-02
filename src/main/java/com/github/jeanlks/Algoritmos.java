d552fdca05fd5901b405de505f2bfd70db16e69f
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-09-01 22:43:21 -0300

    Algoritmos 6 e 7 criados, documentados e testados

--- src/main/java/com/github/jeanlks/Algoritmos.java
+++ src/main/java/com/github/jeanlks/Algoritmos.java
@@ -15,8 +15,8 @@ public class Algoritmos {
 	 * @return soma dos números naturais até n
 	 */
 	public static int somaNaturais(int n) {
-		if(n<=0){
-			 throw new IllegalArgumentException("numero menor que zero");
+		if (n <= 0) {
+			throw new IllegalArgumentException("numero menor que zero");
 		}
 		int i = 2;
 		int s = 1;
@@ -41,8 +41,8 @@ public class Algoritmos {
 	 * @return resultado do produto
 	 */
 	public static int produto(int a, int b) {
-		if(a<=0 || b<=0){
-			 throw new IllegalArgumentException("numero menor que zero");
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("numero menor que zero");
 		}
 		int totalParcelas = a;
 		int parcela = b;
@@ -73,8 +73,8 @@ public class Algoritmos {
 	 * @return resultado da potencia
 	 */
 	public static int potencia(int x, int y) {
-		if(x<=0 || y<=0){
-			 throw new IllegalArgumentException("numero menor que zero");
+		if (x <= 0 || y <= 0) {
+			throw new IllegalArgumentException("numero menor que zero");
 		}
 		int potencia = 1;
 		int i = 1;
@@ -85,4 +85,42 @@ public class Algoritmos {
 		return potencia;
 	}
 
+	/**
+	 * @param n
+	 *            número para calculo
+	 * @param j
+	 *            valor do ultimo numero
+	 * @param i
+	 *            valor do primeiro numero
+	 * 
+	 * @return resultado booleano se o valor corresponde ou nao a propriedade 3025
+	 */
+	public static boolean propriedade3025(int n){
+		if (n<=0 || n>=9999) {
+			throw new IllegalArgumentException("numero menor que zero ou maior que 9999");
+		}	
+		int i = n/100; 
+		int j = n%100;
+		
+		return n == potencia(i+j,2);
+	}
+	
+	/**
+	 * @param n
+	 *            número para calculo
+	 * 
+	 * @return resultado booleano se o valor corresponde ou nao a propriedade 153
+	 */
+	public static boolean propriedade153(int n){
+		if (n<=0 || n>=9999) {
+			throw new IllegalArgumentException("numero menor que zero ou maior que 9999");
+		}	
+		int i = n/100;
+		int j = (n-(100*i))/10;
+		int k = n%10;
+		return n == (potencia(i,3)+potencia(j,3)+potencia(k,3));
+	}
+//	public static void main(String []args){
+//		System.out.println(Algoritmos.propriedade153(1532));
+//	}
 }
--- src/test/java/com/github/jeanlks/AlgoritmosTest.java
+++ src/test/java/com/github/jeanlks/AlgoritmosTest.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 
 public class AlgoritmosTest {
 
+	// Casos de teste para Soma dos Numeros naturais
 	@Test
-	public void numeroCertoParaSoma() {
+	public void valorArbitrarioParaSoma() {
 		assertEquals(6, Algoritmos.somaNaturais(3));
 	}
 
@@ -16,9 +17,10 @@ public class AlgoritmosTest {
 		Algoritmos.somaNaturais(0);
 
 	}
+	// Casos de teste para Produto
 
 	@Test
-	public void numeroCertoParaProduto() {
+	public void valorArbitrarioParaProduto() {
 		assertEquals(9, Algoritmos.produto(3, 3));
 	}
 
@@ -31,30 +33,81 @@ public class AlgoritmosTest {
 	public void numeroMenoresQueZeroSomaProdutoValorA() {
 		Algoritmos.produto(0, 2);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void numeroMenoresQueZeroSomaProdutoValorB() {
 		Algoritmos.produto(3, 0);
 	}
 
+	// Casos de teste para Potencia
+
 	@Test(expected = IllegalArgumentException.class)
-	public void numeroMenoresQueZeroPotenciaValorX() {
+	public void numeroMenorQueZeroPotenciaValorX() {
 
 		Algoritmos.potencia(0, 2);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void numeroMenoresQueZeroPotenciaValorY() {
+	public void numeroMenorQueZeroPotenciaValorY() {
 
 		Algoritmos.potencia(3, 0);
 	}
-	@Test 
-	public void numeroCertoParaPotencia(){
+
+	@Test
+	public void valorArbitrarioParaPotencia() {
 		assertEquals(27, Algoritmos.potencia(3, 3));
+	}
+
+	// Casos de teste para Propriedade 3025
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenorQueZeroPropriedade3025() {
+
+		Algoritmos.propriedade3025(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMaiorQue9999Propriedade3025() {
+
+		Algoritmos.propriedade3025(100000);
+	}
+
+	@Test
+	public void valorCertoParaPropriedade3025() {
+		assertTrue(Algoritmos.propriedade3025(3025));
+	}
+
+	@Test
+	public void valorErradoParaPropriedade3025() {
+		assertFalse(Algoritmos.propriedade3025(3125));
+	}
+
+	// Casos de teste para Propriedade 153
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMenorQueZeroPropriedade153() {
+
+		Algoritmos.propriedade153(0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void numeroMaiorQue9999Propriedade153() {
+
+		Algoritmos.propriedade153(99999);
+	}
+
+	@Test
+	public void valorCertoParaPropriedade153() {
+		assertTrue(Algoritmos.propriedade153(153));
+	}
+
+	@Test
+	public void valorErradoParaPropriedade153() {
+		assertFalse(Algoritmos.propriedade153(154));
 	}
 
 	@Test
 	public void apenasParaAgradarJacocoTool100PorCentoCobertura() {
 		new Algoritmos();
 	}
-}	
+
+}
